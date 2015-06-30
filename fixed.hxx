e9107d74f80323f750fcbb02e48aff44bc0c1140
spl <simon@raspberrypi.org>
2015-06-30 18:12:24 +0100

    First version of code for fixed text accelerators added.

--- fixed.hxx
+++ fixed.hxx
@@ -39,6 +39,7 @@ private:
     sal_Int32 m_nMaxWidthChars;
     sal_Int32 m_nMinWidthChars;
     Window *m_pMnemonicWindow;
+    bool mbShowAccelerator;
 
     using Control::ImplInitSettings;
     using Window::ImplInit;
@@ -80,6 +81,8 @@ public:
     virtual bool set_property(const OString &rKey, const OString &rValue) SAL_OVERRIDE;
     void set_mnemonic_widget(Window *pWindow);
     Window* get_mnemonic_widget() const { return m_pMnemonicWindow; }
+    void                SetShowAccelerator (bool);
+    bool                GetShowAccelerator (void);
 };
 
 class VCL_DLLPUBLIC SelectableFixedText : public Edit
